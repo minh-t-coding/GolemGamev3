603cf59270cd0b65deed321209bd94eb04ce85bd
Simeon Roth <simeonroth@email.arizona.edu>
2017-10-29 04:54:52 -0700

    help

---
+++ Assets/scripts/Transition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour {
+	public void Trans(string scenename)
+	{
+		StartCoroutine(ChangeLevel(scenename));
+	}
+
+	IEnumerator ChangeLevel (string scenename) {
+		float fadeTime = GameObject.Find ("Main Camera").GetComponent<Fading> ().BeginFade (1);
+		yield return new WaitForSeconds (fadeTime);
+		Application.LoadLevel (scenename);
+	}
+}
